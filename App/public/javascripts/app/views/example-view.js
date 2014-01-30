cb00a965b13a6440a4a618fa632069c2e9079fbf
corban baxter <corbanb@gmail.com>
2014-01-29 19:34:23 -0700

    backbone scaffolding

---
+++ App/public/javascripts/app/views/example-view.js
@@ -0,0 +1,18 @@
+var App = App || {};
+
+App.Views = App.Views || {};
+
+(function($) {
+	'use strict';
+
+	App.Views.ExampleView = Backbone.View.extend({
+
+		'events': {	},
+
+		'initialize': function() {
+
+			
+		}
+
+	});
+}(jQuery));
