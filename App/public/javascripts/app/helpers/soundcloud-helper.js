3313846645c044e1f83891ff47715e7ebe4f4bef
corban baxter <corbanb@gmail.com>
2014-01-29 20:13:07 -0700

    sound files

--- App/public/javascripts/app/helpers/soundcloud-helper.js
+++ App/public/javascripts/app/helpers/soundcloud-helper.js
@@ -8,9 +8,30 @@ App.Helpers = App.Helpers || {};
 
 	App.Helpers.SoundCloud = {
 
-		'find': function(type){
+		'find': function(intensity){
+			
+			intensity = Math.floor(intensity);
 
-			return 'http://soundcloud.com/forss/flickermood';
+			var music = {
+				'0': 'http://soundcloud.com/forss/flickermood',
+				'1': 'http://soundcloud.com/forss/flickermood',
+				'2': 'http://soundcloud.com/forss/flickermood',
+				'3': 'http://soundcloud.com/forss/flickermood',
+				'4': 'http://soundcloud.com/forss/flickermood',
+				'5': 'http://soundcloud.com/forss/flickermood',
+				'6': 'http://soundcloud.com/forss/flickermood',
+				'7': 'http://soundcloud.com/forss/flickermood',
+				'8': 'http://soundcloud.com/forss/flickermood',
+				'9': 'http://soundcloud.com/forss/flickermood',
+				'10': 'http://soundcloud.com/forss/flickermood',
+				'11': 'http://soundcloud.com/forss/flickermood',
+				'12': 'http://soundcloud.com/forss/flickermood',
+				'13': 'http://soundcloud.com/forss/flickermood',
+				'14': 'http://soundcloud.com/forss/flickermood'
+
+			};
+
+			return music[intensity];
 
 		}
 
--- App/public/javascripts/app/views/example-view.js
+++ App/public/javascripts/app/views/example-view.js
@@ -15,13 +15,13 @@ App.Views = App.Views || {};
 				client_id: '4f537976a8bc35f6f369aa1b191c2f14'
 			});
 
-			
 			var opts = {
 				auto_play: true,
 				iframe: true
 			};
 
-			var track = App.Helpers.SoundCloud.find(0);
+			var intensity = 5;
+			var track = App.Helpers.SoundCloud.find(intensity);
 
 			SC.oEmbed(track, opts, function(oEmbed) {
 				$('#music').html(oEmbed.html);
